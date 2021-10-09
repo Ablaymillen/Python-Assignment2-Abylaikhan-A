f1b77ae448e7e472a29be0cabb2810964b1aad30
Ablaymillen <80356093+Ablaymillen@users.noreply.github.com>
2021-10-09 17:22:12 +0600

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,1 +1,12 @@
+import sys
+sys.path.append('src/src.py')
+from src.src import Coin
 
+# creating coin object (bitcoin)
+btc = Coin('bitcoin')
+
+# getting crypto news on particular coin (bitcoin)
+btc.get_news()
+
+# getting information on the same coin (bitcoin)
+btc.get_info()
